715b0c05b01999f856c111cefc4905c0764912b8
dongz <89518029@qq.com>
2019-12-26 12:48:55 +0800

    web init

---
+++ hrm_parent/common/src/main/java/com/dongz/hrm/common/entity/PageResult.java
@@ -0,0 +1,16 @@
+package com.dongz.hrm.common.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResult<T> {
+    private Long total;
+    private List<T> rows;
+}
+
