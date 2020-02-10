e3214df9ac99c0ace3c841e1e9a0551cea9bd82e
dongz <89518029@qq.com>
2020-02-10 15:18:45 +0800

    web init

--- code_utils/模板/springboot模板/${path2}_${project}/src/main/java/${path1}/${path2}/${path3}/Application.java
+++ code_utils/模板/springboot模板/${path2}_${project}/src/main/java/${path1}/${path2}/${path3}/Application.java
@@ -9,10 +9,8 @@ import org.springframework.context.annotation.Bean;
 /**
  * @author dong
  * @date 2019/12/26 13:45
- * @desc EntityScan  jps 包扫描
  */
-@SpringBootApplication(scanBasePackages = "com.dongz")
-@EntityScan(value = "com.dongz.hrm.domain.company")
+@SpringBootApplication(scanBasePackages = "${path1}.${path2}")
 public class Application {
 
     public static void main(String[] args) {
