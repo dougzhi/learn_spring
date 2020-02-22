e0ead3b28d6866f4ea1a4c6cdb36b8a6b918f99d
dongz <89518029@qq.com>
2020-02-22 12:13:00 +0800

    web init

--- hrm_parent/system/src/main/java/com/dongz/hrm/system/services/PermissionService.java
+++ hrm_parent/system/src/main/java/com/dongz/hrm/system/services/PermissionService.java
@@ -42,6 +42,8 @@ public class PermissionService extends BaseService {
         Assert.hasText(vo.getName(), "权限名称不能为空");
         Assert.hasText(vo.getCode(), "权限码不能为空");
         Assert.notNull(vo.getPid(), "父级权限不能为空");
+        Assert.notNull(vo.getIsVisible(), "权限可见性不能为空");
+
         PermissionStatus permissionStatus = PermissionStatus.parse(vo.getType());
         if (permissionStatus.equals(PermissionStatus.API)) {
             Assert.hasText(vo.getApiUrl(), "链接不能为空");
@@ -83,22 +85,6 @@ public class PermissionService extends BaseService {
         return permissionId;
     }
 
-    private void permissionOption(Object vo,String option, String className) {
-        try {
-            Class<PermissionService> pClazz = PermissionService.class;
-            Class<?>[] declaredClasses = pClazz.getDeclaredClasses();
-            Optional<Class<?>> first = Arrays.stream(declaredClasses).filter(item -> item.getSimpleName().equals(className)).findFirst();
-            Assert.isTrue(first.isPresent(), "方法未找到");
-
-            Class<?> aClass = first.get();
-            Method method = aClass.getDeclaredMethod(option, Object.class, EntityManager.class);
-            method.invoke(aClass.getDeclaredConstructors()[0].newInstance(pClazz.newInstance()), vo, em);
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
-            Assert.isTrue(false, e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
     /**
      * 修改
      * @param vo vo
@@ -107,11 +93,27 @@ public class PermissionService extends BaseService {
         Assert.notNull(vo, "权限信息不能为空");
         Assert.notNull(vo.getId(), "要修改的权限ID不能为空");
         Assert.hasText(vo.getName(), "权限名称不能为空");
+        Assert.hasText(vo.getCode(), "权限码不能为空");
+        Assert.notNull(vo.getIsVisible(), "权限可见性不能为空");
+
+        PermissionStatus permissionStatus = PermissionStatus.parse(vo.getType());
+        if (permissionStatus.equals(PermissionStatus.API)) {
+            Assert.hasText(vo.getApiUrl(), "链接不能为空");
+            Assert.hasText(vo.getApiMethod(), "链接请求类型不能为空");
+            Assert.hasText(vo.getApiLevel(), "链接权限等级不能为空");
+        } else if (permissionStatus.equals(PermissionStatus.MENU)) {
+            Assert.hasText(vo.getMenuIcon(), "菜单图标不能为空");
+            Assert.hasText(vo.getMenuOrder(), "菜单排序码不能为空");
+        } else {
+            Assert.hasText(vo.getPointIcon(), "权限点图标不能为空");
+            Assert.hasText(vo.getPointClass(), "权限代码不能为空");
+            Assert.hasText(vo.getPointStatus(), "权限点状态不能为空");
+        }
 
         Permission permission = em.find(Permission.class, vo.getId());
         Assert.notNull(permission, "权限信息不存在， 修改失败");
+        Assert.isTrue(permissionStatus.equals(permission.getType()), "权限类型禁止修改");
 
-        // 权限手机号码
         if (!permission.getName().equals(vo.getName())) {
             Optional<Permission> first = em.createQuery("select u from Permission u where u.name = ?1 ", Permission.class).setParameter(1, vo.getName()).getResultStream().findFirst();
             Assert.isTrue((!first.isPresent()) || (first.get().getName().equals(vo.getName())), "权限名称重复， 修改失败");
@@ -119,8 +121,12 @@ public class PermissionService extends BaseService {
         }
 
         permission.setDescription(vo.getDescription());
+        permission.setCode(vo.getCode());
+        permission.setIsVisible(Boolean.parseBoolean(vo.getIsVisible()));
 
         em.merge(permission);
+
+        permissionOption(vo, "update", permissionStatus.getClassName());
     }
 
     /**
@@ -141,6 +147,22 @@ public class PermissionService extends BaseService {
         em.remove(permission);
     }
 
+    private void permissionOption(Object vo,String option, String className) {
+        try {
+            Class<PermissionService> pClazz = PermissionService.class;
+            Class<?>[] declaredClasses = pClazz.getDeclaredClasses();
+            Optional<Class<?>> first = Arrays.stream(declaredClasses).filter(item -> item.getSimpleName().equals(className)).findFirst();
+            Assert.isTrue(first.isPresent(), "方法未找到");
+
+            Class<?> aClass = first.get();
+            Method method = aClass.getDeclaredMethod(option, Object.class, EntityManager.class);
+            method.invoke(aClass.getDeclaredConstructors()[0].newInstance(pClazz.newInstance()), vo, em);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            Assert.isTrue(false, e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
     @Transactional
     class PermissionApiService {
         public void create(Object vo, EntityManager em) {
@@ -153,6 +175,19 @@ public class PermissionService extends BaseService {
             api.setApiLevel(permissionVO.getApiLevel());
 
             em.persist(api);
+        }
+
+        public void update(Object vo, EntityManager em) {
+            PermissionVO permissionVO = (PermissionVO) vo;
+
+            PermissionApi api = em.find(PermissionApi.class, permissionVO.getId());
+            Assert.notNull(api, "权限链接信息不能为空");
+
+            api.setApiUrl(permissionVO.getApiUrl());
+            api.setApiMethod(permissionVO.getApiMethod());
+            api.setApiLevel(permissionVO.getApiLevel());
+
+            em.merge(api);
         }
 
         public void delete(Object id, EntityManager em) {
@@ -174,6 +209,18 @@ public class PermissionService extends BaseService {
             em.persist(menu);
         }
 
+        public void update(Object vo, EntityManager em) {
+            PermissionVO permissionVO = (PermissionVO) vo;
+
+            PermissionMenu menu = em.find(PermissionMenu.class, permissionVO.getId());
+            Assert.notNull(menu, "权限菜单信息不能为空");
+
+            menu.setMenuIcon(permissionVO.getMenuIcon());
+            menu.setMenuOrder(permissionVO.getMenuOrder());
+
+            em.merge(menu);
+        }
+
         public void delete(Object id, EntityManager em) {
             Long permissionId = (Long) id;
             em.remove(em.getReference(PermissionMenu.class, permissionId));
@@ -192,6 +239,18 @@ public class PermissionService extends BaseService {
             point.setPointStatus(permissionVO.getPointStatus());
 
             em.persist(point);
+        }
+
+        public void update(Object vo, EntityManager em) {
+            PermissionVO permissionVO = (PermissionVO) vo;
+            PermissionPoint point = em.find(PermissionPoint.class, permissionVO.getId());
+            Assert.notNull(point, "权限按钮信息不能为空");
+
+            point.setPointClass(permissionVO.getPointClass());
+            point.setPointIcon(permissionVO.getPointIcon());
+            point.setPointStatus(permissionVO.getPointStatus());
+
+            em.merge(point);
         }
 
         public void delete(Object id, EntityManager em) {
