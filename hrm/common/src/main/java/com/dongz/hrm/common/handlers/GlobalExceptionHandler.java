921a1be351a84e6027ebb7e3bf06c9a874712c6a
dongz <89518029@qq.com>
2020-02-24 13:51:03 +0800

    验证shrio

--- hrm/common/src/main/java/com/dongz/hrm/common/handlers/GlobalExceptionHandler.java
+++ hrm/common/src/main/java/com/dongz/hrm/common/handlers/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.dongz.hrm.common.handlers;
 
 import com.dongz.hrm.common.entities.Result;
 import com.dongz.hrm.common.entities.ResultCode;
+import org.apache.shiro.authz.AuthorizationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -19,5 +20,11 @@ public class GlobalExceptionHandler {
     public final ResponseEntity<Result> handlerException(Exception e) {
         e.printStackTrace();
         return new ResponseEntity<>(Result.FAILE(ResultCode.FAILE.code(), e.getMessage()), HttpStatus.OK);
+    }
+
+    @ExceptionHandler(AuthorizationException.class)
+    public final ResponseEntity<Result> handlerAuthorizationException(Exception e) {
+        e.printStackTrace();
+        return new ResponseEntity<>(Result.FAILE(ResultCode.UNAUTHORISE.code(), "权限不足"), HttpStatus.OK);
     }
 }
