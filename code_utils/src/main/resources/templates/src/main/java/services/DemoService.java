42e0decfb7c180c162c7b8b43b7e86af572100d0
dongz <89518029@qq.com>
2020-03-02 10:31:13 +0800

    

--- code_utils/src/main/resources/templates/src/main/java/services/DemoService.java
+++ code_utils/src/main/resources/templates/src/main/java/services/DemoService.java
@@ -1,7 +1,7 @@
 package ${pPackage}.services;
 
 import ${pPackage}.entities.${ClassName};
-<#if hasVO=true>
+<#if hasVO>
 import ${pPackage}.entities.vos.${ClassName}VO;
 </#if>
 <#list foreignTables as foreignTable>
@@ -30,6 +30,7 @@ public class ${ClassName}Service extends BaseService {
     @Autowired
     private IdWorker idWorker;
 
+    <#if hasVO>
     /**
      * 新增
      *
@@ -39,7 +40,7 @@ public class ${ClassName}Service extends BaseService {
         Assert.notNull(vo, "${table.comment?default('xxx')}不能为空");
         //断言
         <#list table.columns as column>
-        <#if column.notNull=true && !baseEntity?contains(column.fieldName)>
+        <#if column.selected && column.notNull && !baseEntity?contains(column.fieldName) && column.fieldName !== 'id'>
         <#if column.columnType=='String'>
         Assert.hasText(vo.${column.getName}(), "${column.columnComment?default('xxx')}不能为空");
         <#else>
@@ -50,7 +51,7 @@ public class ${ClassName}Service extends BaseService {
 
         //唯一
         <#list table.columns as column>
-        <#if column.only=true>
+        <#if column.selected && column.only>
         long count = em.createQuery("select count(1) from ${table.className} u where u.${column.fieldName} = ?1 <#if table.extendsBase>and u.isDeleted = false</#if>", Long.class).setParameter(1, vo.${column.getName}()).getSingleResult();
         Assert.isTrue(count == 0, "${column.columnComment}?default('xxx')重复， 新增失败");
         </#if>
@@ -58,7 +59,7 @@ public class ${ClassName}Service extends BaseService {
 
         //外键
         <#list table.columns as column>
-        <#if column.foreignColumn??>
+        <#if column.selected && column.foreignColumn??>
         long count = em.createQuery("select count(1) from ${column.foreignColumn.table.className} u where u.${column.foreignColumn.column.fieldName} = ?1 <#if table.extendsBase>and u.isDeleted = false</#if>", Long.class).setParameter(1, vo.${column.getName}()).getSingleResult();
         Assert.isTrue(count == 0, "${column.columnComment}?default('xxx')外键关联不存在， 新增失败");
         </#if>
@@ -72,36 +73,158 @@ public class ${ClassName}Service extends BaseService {
         em.persist(entity);
         return entity.getId();
     }
+    <#else>
+    /**
+     * 新增
+     *
+     */
+    public Long create(<#list table.columns as column><#if column.selected && !baseEntity?contains(column.fieldName) && column.fieldName !== 'id'>${column.columnType} ${column.fieldName},</#if></#list>) {
+        //断言
+        <#list table.columns as column>
+        <#if column.selected && column.notNull=true && !baseEntity?contains(column.fieldName) && column.fieldName !== 'id'>
+        <#if column.columnType=='String'>
+        Assert.hasText(${column.fieldName}, "${column.columnComment?default('xxx')}不能为空");
+        <#else>
+        Assert.notNull(${column.fieldName}, "${column.columnComment?default('xxx')}不能为空");
+        </#if>
+        </#if>
+        </#list>
 
+        //唯一
+        <#list table.columns as column>
+        <#if column.selected && column.only>
+        long count = em.createQuery("select count(1) from ${table.className} u where u.${column.fieldName} = ?1 <#if table.extendsBase>and u.isDeleted = false</#if>", Long.class).setParameter(1, ${column.fieldName}).getSingleResult();
+        Assert.isTrue(count == 0, "${column.columnComment}?default('xxx')重复， 新增失败");
+        </#if>
+        </#list>
+
+        //外键
+        <#list table.columns as column>
+        <#if column.selected && column.foreignColumn??>
+        long count = em.createQuery("select count(1) from ${column.foreignColumn.table.className} u where u.${column.foreignColumn.column.fieldName} = ?1 <#if table.extendsBase>and u.isDeleted = false</#if>", Long.class).setParameter(1, ${column.fieldName}).getSingleResult();
+        Assert.isTrue(count == 0, "${column.columnComment}?default('xxx')外键关联不存在， 新增失败");
+        </#if>
+        </#list>
+
+        ${table.className} entity = new ${table.className}();
+
+        entity.setId(idWorker.nextId());
+        <#list table.columns as column>
+        <#if column.selected && !baseEntity?contains(column.fieldName)>
+        entity.${column.setName}(${column.fieldName});
+        </#if>
+        </#list>
+
+        setCreate(entity);
+        em.persist(entity);
+        return entity.getId();
+    }
+    </#if>
+
+    <#if hasVO>
     /**
      * 修改
      *
      * @param vo vo
      */
     public void update(${ClassName}VO vo) {
-        Assert.notNull(vo, "用户信息不能为空");
-        Assert.notNull(vo.getId(), "要修改的用户ID不能为空");
-        Assert.hasText(vo.getUsername(), "用户名称不能为空");
-        Assert.hasText(vo.getMobile(), "用户手机号码不能为空");
+        Assert.notNull(vo, "${table.comment?default('xxx')}不能为空");
+        //断言
+        <#list table.columns as column>
+        <#if column.selected && column.notNull && !baseEntity?contains(column.fieldName)>
+        <#if column.columnType=='String'>
+        Assert.hasText(vo.${column.getName}(), "${column.columnComment?default('xxx')}不能为空");
+        <#else>
+        Assert.notNull(vo.${column.getName}(), "${column.columnComment?default('xxx')}不能为空");
+        </#if>
+        </#if>
+        </#list>
 
-        User user = em.find(User.class, vo.getId());
-        Assert.notNull(user, "用户信息不存在， 修改失败");
-        Assert.isTrue(!user.isDeleted(), "用户信息已删除");
+        ${table.className} entity = em.find(${table.className}.class, vo.getId());
+        Assert.notNull(entity, "${table.comment?default('xxx')}不存在， 修改失败");
+        <#if table.extendsBase>
+        Assert.isTrue(!entity.isDeleted(), "${table.comment?default('xxx')}已删除");
+        </#if>
 
-        // 用户手机号码
-        if (!user.getMobile().equals(vo.getMobile())) {
-            Optional<User> first = em.createQuery("select u from User u where u.mobile = ?1 and u.isDeleted = false ", User.class).setParameter(1, vo.getMobile()).getResultStream().findFirst();
-            Assert.isTrue((!first.isPresent()) || (first.get().getMobile().equals(vo.getMobile())), "用户手机号码重复， 修改失败");
-            user.setMobile(vo.getMobile());
+        // 唯一
+        <#list table.columns as column>
+        <#if column.selected && column.only>
+        if (!entity.${column.getName}().equals(vo.${column.getName}())) {
+            Optional<${table.className}> optional = em.createQuery("select u from ${table.className} u where u.${column.fieldName} = ?1 <#if table.extendsBase>and u.isDeleted = false</#if>", ${table.className}.class).setParameter(1, vo.${column.getName}()).getResultStream().findFirst();
+            Assert.isTrue((!optional.isPresent()) || (optional.get().${column.getName}().equals(vo.${column.getName}())), "${column.columnComment}?default('xxx')重复， 修改失败");
+            entity.${column.setName}(vo.${column.getName}());
         }
+        </#if>
+        </#list>
 
-        user.setUsername(vo.getUsername());
-        user.setEnableState(EnableState.Enable);
-        user.setFormOfEmployment(FormOfEmployment.Formal);
+        //外键
+        <#list table.columns as column>
+        <#if column.selected && column.foreignColumn??>
+        long count = em.createQuery("select count(1) from ${column.foreignColumn.table.className} u where u.${column.foreignColumn.column.fieldName} = ?1 <#if table.extendsBase>and u.isDeleted = false</#if>", Long.class).setParameter(1, ${column.fieldName}).getSingleResult();
+        Assert.isTrue(count == 0, "${column.columnComment}?default('xxx')外键关联不存在， 新增失败");
+        </#if>
+        </#list>
 
-        setLastUpdate(user);
-        em.merge(user);
+        <#list table.columns as column>
+        <#if column.selected && !baseEntity?contains(column.fieldName) && column.fieldName !== 'id'>
+        entity.${column.setName}(vo.${column.getName}());
+        </#if>
+        </#list>
+
+        setLastUpdate(entity);
+        em.merge(entity);
     }
+    <#else>
+    /**
+     * 修改
+     *
+     */
+    public void update(<#list table.columns as column><#if column.selected && !baseEntity?contains(column.fieldName)>${column.columnType} ${column.fieldName},</#if></#list>) {
+        //断言
+        <#list table.columns as column>
+        <#if column.selected && column.notNull=true && !baseEntity?contains(column.fieldName) && column.fieldName !== 'id'>
+        <#if column.columnType=='String'>
+        Assert.hasText(${column.fieldName}, "${column.columnComment?default('xxx')}不能为空");
+        <#else>
+        Assert.notNull(${column.fieldName}, "${column.columnComment?default('xxx')}不能为空");
+        </#if>
+        </#if>
+        </#list>
+
+        ${table.className} entity = em.find(${table.className}.class, id);
+        Assert.notNull(entity, "${table.comment?default('xxx')}不存在， 修改失败");
+        <#if table.extendsBase>
+        Assert.isTrue(!entity.isDeleted(), "${table.comment?default('xxx')}已删除");
+        </#if>
+
+        // 唯一
+        <#list table.columns as column>
+        <#if column.selected && column.only>
+        if (!entity.${column.getName}().equals(${column.fieldName})) {
+        Optional<${table.className}> optional = em.createQuery("select u from ${table.className} u where u.${column.fieldName} = ?1 <#if table.extendsBase>and u.isDeleted = false</#if>", ${table.className}.class).setParameter(1, ${column.fieldName}).getResultStream().findFirst();
+        Assert.isTrue((!optional.isPresent()) || (optional.get().${column.getName}().equals(${column.fieldName})), "${column.columnComment}?default('xxx')重复， 修改失败");
+        entity.${column.setName}(${column.fieldName});
+        }
+        </#if>
+        </#list>
+
+        //外键
+        <#list table.columns as column>
+        <#if column.selected && column.foreignColumn??>
+        long count = em.createQuery("select count(1) from ${column.foreignColumn.table.className} u where u.${column.foreignColumn.column.fieldName} = ?1 <#if table.extendsBase>and u.isDeleted = false</#if>", Long.class).setParameter(1, ${column.fieldName}).getSingleResult();
+        Assert.isTrue(count == 0, "${column.columnComment}?default('xxx')外键关联不存在， 新增失败");
+        </#if>
+        </#list>
+
+        <#list table.columns as column>
+        <#if column.selected && !baseEntity?contains(column.fieldName) && column.fieldName !== 'id'>
+        entity.${column.setName}(${column.fieldName});
+        </#if>
+        </#list>
+
+        setLastUpdate(entity);
+        em.merge(entity);
+        }
 
     /**
      * 删除
@@ -109,50 +232,17 @@ public class ${ClassName}Service extends BaseService {
      * @param id id
      */
     public void delete(Long id) {
-        Assert.notNull(id, "要删除的用户ID不能为空");
+        Assert.notNull(id, "要删除的${table.comment?default('xxx')}ID不能为空");
 
-        User user = em.find(User.class, id);
-        Assert.notNull(user, "用户信息不存在， 修改失败");
-        Assert.isTrue(!user.isDeleted(), "用户信息已删除");
+        ${table.className} entity = em.find(${table.className}.class, id);
+        Assert.notNull(entity, "${table.comment?default('xxx')}不存在， 删除失败");
+        <#if table.extendsBase>
+        Assert.isTrue(!entity.isDeleted(), "${table.comment?default('xxx')}已删除");
 
-        //删除角色关联
-        List<UserRole> resultList = em.createQuery("select u from UserRole u where u.userId = ?1", UserRole.class).setParameter(1, id).getResultList();
-        resultList.forEach(item -> em.remove(item));
-
-        setDelete(user);
-        em.merge(user);
-    }
-
-    /**
-     * 授权
-     *
-     * @param id id
-     */
-    public void assignRoles(Long id, List<Long> roleIds) {
-        Assert.notNull(id, "要授权的用户ID不能为空");
-
-        User user = em.find(User.class, id);
-        Assert.notNull(user, "用户信息不存在， 修改失败");
-        Assert.isTrue(!user.isDeleted(), "用户信息已删除");
-
-        long count = em.createQuery("select count(1) from Role u where u.id in (:ids) and u.isDeleted = false", Long.class).setParameter("ids", roleIds).getSingleResult();
-        Assert.isTrue(count == roleIds.size(), "角色信息异常");
-
-        List<UserRole> list = em.createQuery("select u from UserRole u where u.userId = :id", UserRole.class).setParameter("id", id).getResultList();
-        // 取左差集，新增
-        List<Long> roleIdList = list.parallelStream().map(UserRole::getRoleId).collect(Collectors.toList());
-        List<Long> createList = roleIds.parallelStream().filter(item -> !roleIdList.contains(item)).collect(Collectors.toList());
-        // 取右差集，删除
-        List<UserRole> removeList = list.parallelStream().filter(item -> !roleIds.contains(item.getRoleId())).collect(Collectors.toList());
-
-        createList.forEach(item -> {
-            UserRole userRole = new UserRole(id, item);
-            em.persist(userRole);
-        });
-        removeList.forEach(item -> em.remove(item));
-
-        if ((createList.size() + removeList.size()) > 0) {
-            SystemSessionManager.reloadAuthorizing(authService.getProfile(user));
-        }
+        setDelete(entity);
+        em.merge(entity);
+        <#else>
+        em.remove(entity);
+        </if>
     }
 }
