51138dac6ccf4b3d0e651b3c69d57027921d86c5
dongz <89518029@qq.com>
2020-03-24 09:53:34 +0800

    

---
+++ learn_activiti/src/main/java/com/dongz/learn_activiti/LearnActivitiApplication.java
@@ -0,0 +1,13 @@
+package com.dongz.learn_activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnActivitiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnActivitiApplication.class, args);
+	}
+
+}
---
+++ learn_activiti/src/test/java/com/dongz/learn_activiti/LearnActivitiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongz.learn_activiti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnActivitiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
